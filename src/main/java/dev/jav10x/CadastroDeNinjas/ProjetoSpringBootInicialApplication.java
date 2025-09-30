68233e601ba2aa6e82c315f8964b9c725b610f1d
Gustavo <cesarfilhoandrade8@gmail.com>
2025-09-29 22:22:44 -0300

    first commit

---
+++ src/main/java/dev/jav10x/CadastroDeNinjas/ProjetoSpringBootInicialApplication.java
@@ -0,0 +1,13 @@
+package dev.jav10x.CadastroDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBootInicialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBootInicialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jav10x/CadastroDeNinjas/ProjetoSpringBootInicialApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jav10x.CadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringBootInicialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
