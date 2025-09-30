7496f5ef68f467f99259055989dedc204bf731b9
Gustavo <cesarfilhoandrade8@gmail.com>
2025-09-30 19:32:59 -0300

    Controller created

---
+++ src/main/java/dev/jav10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package dev.jav10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller{
+
+    @GetMapping("/welcome") //Isso faz com que o método "welcome()" seja executado quando uma solicitação HTTP Get seja feita na URL/welcome
+    public String welcome(){
+        return "Welcome to this route";
+    }
+}
