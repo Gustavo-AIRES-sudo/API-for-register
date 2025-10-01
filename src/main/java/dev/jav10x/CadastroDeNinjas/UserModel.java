39dc5c0d823c6d15357a259f564e7ad2990a9cb1
Gustavo <cesarfilhoandrade8@gmail.com>
2025-09-30 21:05:30 -0300

    changed version of java to 21; import of JPA and aplication of UserModel on project

---
+++ src/main/java/dev/jav10x/CadastroDeNinjas/UserModel.java
@@ -0,0 +1,56 @@
+package dev.jav10x.CadastroDeNinjas;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity //Transforma a classe em uma entidade da database. Tabela:
+@Table(name = "tb_user_register") //Responsável por criar a Tabela
+public class UserModel {
+
+    Long ID;
+    String name;
+    String surname;
+    String gmail;
+    int age;
+
+    public UserModel(String name, String surname, String gmail, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.gmail = gmail;
+        this.age = age;
+    }
+
+    public UserModel() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getGmail() {
+        return gmail;
+    }
+
+    public void setGmail(String gmail) {
+        this.gmail = gmail;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
